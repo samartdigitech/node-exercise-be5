7ee84c09f15d1136a699035be10b3e476a63c9c8
SAMART NUANPAKSEE <admin@samartdigitech.com>
2026-06-29 18:32:42 +0700

    fix: exercise

--- creating-records-exercise/app.mjs
+++ creating-records-exercise/app.mjs
@@ -7,6 +7,28 @@ const port = 4000;
 app.use(express.json());
 
 // 📍 **** สร้าง API เพื่อใช้ในการเพิ่มข้อมูลหนังเรื่องใหม่ไปที่ Database ตรงนี้ ****
+app.post("/movies", async (req,res)=>{
+
+	await pool.query(
+		`insert into movies (title, description, genres, year, poster, rating)
+		values ($1,$2,$3,$4,$5,$6) RETURNING *`,
+		[
+			
+			req.body.title,
+			req.body.description,
+			req.body.genres,
+			req.body.year,
+			req.body.poster,
+			req.body.rating
+
+		]
+	)
+
+	return res.status(201).json({
+		message : "Movie has been created."
+	})
+})
+
 
 app.listen(port, () => {
 	console.log(`🚀 Server is running at ${port}`);
