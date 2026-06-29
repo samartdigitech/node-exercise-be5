933287d49c2476c3dfb2d959345c49079e4283d8
SAMART NUANPAKSEE <admin@samartdigitech.com>
2026-06-29 22:34:09 +0700

    feat: exercise

--- querying-records-exercise/app.mjs
+++ querying-records-exercise/app.mjs
@@ -15,6 +15,23 @@ app.get("/movies", async (req, res) => {
 });
 
 // 📍 **** สร้าง API เพื่อใช้ในการดูข้อมูลหนังแต่ละเรื่องด้วย movieId ตรงนี้ ****
+app.get("/movies/:movieId" , async (req,res) => {
+	const movieIdFromClient = req.params.movieId
+	let results
+	try{
+		results = await pool.query (
+		`select * from movies where movie_id = $1`,[movieIdFromClient]
+	)
+	}catch{
+		return res,status(500).json({
+			message : "ไม่สามารถเชื่อมต่อ Database ได้"
+		})
+	}
+	return res.status(200).json({
+		message: "Movie has been created.",
+		data: results.rows
+	})
+})
 
 app.listen(port, () => {
 	console.log(`🚀 Server is running at ${port}`);
