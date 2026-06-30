2c8062f99a74605547e8b3b68ee7bae1f5be0b5e
SAMART NUANPAKSEE <admin@samartdigitech.com>
2026-06-30 10:19:11 +0700

    feat: exercise querying-all-records-with-query-parameters

--- querying-all-records-with-query-parameters-exercise-1/app.mjs
+++ querying-all-records-with-query-parameters-exercise-1/app.mjs
@@ -9,20 +9,22 @@ app.use(express.json());
 app.get("/movies", async (req, res) => {
 	try {
 		// แก้ไขโค้ดให้สามารถกรองผลลัพธ์ด้วย Parameter ได้ข้างล่างนี้ 🔽🔽🔽
-		const genresParam = null;
+		const genres = req.query.genres;
 		const result = await pool.query(
 			`
     SELECT * FROM movies
+	where (genres = $1 or $1 is null or $1 = '')
     `,
-			[]
+			[genres]
 		);
 		// แก้ไขโค้ดให้สามารถกรองผลลัพธ์ด้วย Parameter ได้ข้างบนนี้ 🔼🔼🔼
 
-		return res.json({
+		return res.status(200).json({
 			data: result.rows,
 		});
 	} catch (e) {
-		return res.json({
+		console.log("Database Error :",e)
+		return res.status(500).json({
 			message: "ไม่สามารถเชื่อมต่อ Database ได้",
 		});
 	}
